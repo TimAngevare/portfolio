6aa55207e0020322ba11a244d9d43b5d793c6d52
s2744007 <t.p.angevare@student.utwente.nl>
2023-10-17 15:42:27 +0200

    Added 2 new projects

--- src/index.js
+++ src/index.js
@@ -11,9 +11,13 @@ import PostSection from "./Components/PostSection";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <main className="container" data-theme="dark">
         <div>
+            <div id="google_translate_element"></div>
+            <script type="text/javascript"
+                    src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
             <div>
                 <Nav/>
             </div>
